aa1e40f030dc0ec5aadce65214cb61a3f3dfeaf0
razorenova <razorenova.om@yandex.ru>
2021-04-13 11:33:34 +0300

    table

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,23 +1,33 @@
+
 import React from 'react';
+import {Link} from "react-router-dom";
 
-function PreviewPost(){
-    return(
-        <div className="post-preview">
-            <a href="post.html">
-                <h2 className="post-title">
-                    Man must explore, and this is exploration at its greatest
-                </h2>
-                <h3 className="post-subtitle">
-                    Problems look mighty small from 150 miles up
-                </h3>
-            </a>
-            <p className="post-meta">Posted by
-                <a href="#">Start Bootstrap</a>
-                on September 24, 2019</p>
-        </div>
+function Tr(props) {
+    return <tr>
+        <th scope="row">{props.index} [{props.id}]</th>
+        <td><Link to={"/post/" + props.id}>{props.title}</Link></td>
+        <td>{props.author}</td>
+        <td>{props.data_added}</td>
+        <td><span className='delete-post-btn' onClick={()=> {
+            const formData = new FormData();
+            formData.append('id',props.id);
+            fetch ("http://v90377xk.beget.tech/pre/php/removePost.php", {
+                method: "POST",
+                body: formData
+            }).then(response=>response.json())
+                .then(result=>{
+                    let posts = props.parent.state.posts;
+                    posts.splice(props.index-1,1 ) ;
+                    props.parent.setState({
+                        posts:posts
+                    })
+                })
+        }
+        }>[Удалить]</span></td>
 
-    )
+    </tr>
 }
+
 export class PostList extends React.Component{
     constructor() {
         super();
@@ -26,28 +36,48 @@ export class PostList extends React.Component{
         }
     }
     componentDidMount() {
-        fetch('https://v90377xk/beget.tech/pre/php/getPosts.php')
-            .then(response=>response.json ())
+        console.log("Компонент PostList отрисован");
+
+        fetch("http://v90377xk.beget.tech/pre/php/getPosts.php")
+
+            .then(response=>response.json())
             .then(result=>{
-                this.setState({
-                    posts: result.map(item=><PreviewPost/>)
-                    }
-                )
+                console.log(result);
+                let rows = [];
+                for (let i = 0; i < result.length; i++) {
+                    rows.push(<Tr
+                        key={i}
+                        index={ i + 1}
+                        id={result[i].id}
+                        title={result[i].title}
+                        author={result[i].author}
+                        data_added={result[i].data_added}
+                        parent={this}
+                    />)
+                }
+                this.setState( {
+                    posts: rows
+                })
+
             })
     }
 
-    render(){
-        return(
+    render() {
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-10 mx-auto">
-                        {this.state.posts}
-                    </div>
-                </div>
-            </div>
-
-
-        )
+        console.log("Компонент PostList рисуется")
+        return <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Заголовок</th>
+                <th scope="col">Автор</th>
+                <th scope="col">Дата добавления</th>
+                <th scope="col">Управление</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.posts}
+            </tbody>
+        </table>
     }
 }
